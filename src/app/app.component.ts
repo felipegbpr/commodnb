723a053650982c2789f0cf46b87696a117091036
Felipe Borges <felipetecacc@gmail.com>
2024-10-08 14:24:26 -0300

    Initial layout footer and navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,23 @@ import { ButtonModule } from "primeng/button";
 import { RouterOutlet } from '@angular/router';
 import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { ToastModule } from "primeng/toast";
+import { MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, ButtonModule, FontAwesomeModule],
+  imports: [RouterOutlet, ButtonModule, NavbarComponent, FooterComponent, FontAwesomeModule, ToastModule],
+  providers: [MessageService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
 
   faIConLibrary = inject(FaIconLibrary);
+  isListingView = true;
 
   ngOnInit(): void {
     this.initFontAwesome();
--- src/app/layout/navbar/avatar/avatar.component.ts
+++ src/app/layout/navbar/avatar/avatar.component.ts
@@ -1,16 +1,20 @@
-import { NgClass } from '@angular/common';
-import { Component, input } from '@angular/core';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import {Component, input} from '@angular/core';
+import {NgClass} from "@angular/common";
+import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 
 @Component({
   selector: 'app-avatar',
   standalone: true,
-  imports: [NgClass, FontAwesomeModule],
+  imports: [
+    NgClass,
+    FontAwesomeModule
+  ],
   templateUrl: './avatar.component.html',
   styleUrl: './avatar.component.scss'
 })
 export class AvatarComponent {
 
   imageUrl = input<string>();
-  avatarSize = input<"avatar-sm" | "avatar-xl">;
+  avatarSize = input<"avatar-sm" | "avatar-xl">();
+
 }
