e5a4f2fc82635d7b430aff0cf71b16b9f74bf4ee
Felipe Borges <felipetecacc@gmail.com>
2025-12-06 15:18:05 -0300

    feat: add search integration

--- src/app/layout/navbar/navbar.component.ts
+++ src/app/layout/navbar/navbar.component.ts
@@ -11,6 +11,9 @@ import { MenuItem } from 'primeng/api';
 import { ToastService } from '../toast.service';
 import { User } from '../../core/model/user.model';
 import { PropertiesCreateComponent } from '../../landlord/properties-create/properties-create.component';
+import { SearchComponent } from '../../tenant/search/search.component';
+import { ActivatedRoute } from '@angular/router';
+import dayjs from 'dayjs';
 
 @Component({
   selector: 'app-navbar',
@@ -35,6 +38,7 @@ export class NavbarComponent implements OnInit {
   toastService = inject(ToastService);
   authService = inject(AuthService);
   dialogService = inject(DialogService);
+  activatedRoute = inject(ActivatedRoute);
   ref: DynamicDialogRef | undefined;
 
   login = () => this.authService.login();
@@ -56,6 +60,7 @@ export class NavbarComponent implements OnInit {
 
   ngOnInit(): void {
     this.authService.fetch(false);
+    this.extractInformationForSearch();
   }
 
   private fetchMenu(): MenuItem[] {
@@ -99,15 +104,44 @@ export class NavbarComponent implements OnInit {
     return this.authService.hasAnyAuthority('ROLE_LANDLORD');
   }
 
- openNewListing(): void {
-    this.ref = this.dialogService.open(PropertiesCreateComponent,
-      {
-        width: "60%",
-        header: "Commondnb your home",
-        closable: true,
-        focusOnShow: true,
-        modal: true,
-        showHeader: true
-      })
+  openNewListing(): void {
+    this.ref = this.dialogService.open(PropertiesCreateComponent, {
+      width: '60%',
+      header: 'Commondnb your home',
+      closable: true,
+      focusOnShow: true,
+      modal: true,
+      showHeader: true,
+    });
+  }
+
+  openNewSearch(): void {
+    this.ref = this.dialogService.open(SearchComponent, {
+      width: '40%',
+      header: 'Search',
+      closable: true,
+      focusOnShow: true,
+      modal: true,
+      showHeader: true,
+    });
+  }
+
+  private extractInformationForSearch(): void {
+    this.activatedRoute.queryParams.subscribe({
+      next: (params) => {
+        if (params['location']) {
+          this.location = params['location'];
+          this.guests = params['guests'] + ' Guests';
+          this.dates =
+            dayjs(params['startDate']).format('MMM-DD') +
+            ' to ' +
+            dayjs(params['endDate']).format('MMM-DD');
+        } else if (this.location !== 'Anywhere') {
+          this.location = 'Anywhere';
+          this.guests = 'Add guests';
+          this.dates = 'Any week';
+        }
+      },
+    });
   }
 }
